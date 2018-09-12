9dfb8af4232d78177b6e7b6d283a7404031b8358
Rahul B <rahulbot@gmail.com>
2018-09-12 16:35:58 -0400

    Refactor permissions as fieldArray (#1235)

--- server/views/topics/permissions.py
+++ server/views/topics/permissions.py
@@ -2,6 +2,7 @@
 import logging
 from flask import jsonify, request
 import flask_login
+import json
 from mediacloud.error import MCException
 
 from server import app
@@ -9,6 +10,7 @@ from server.util.request import form_fields_required, json_error_response, api_e
 from server.auth import user_admin_mediacloud_client
 
 logger = logging.getLogger(__name__)
+
 
 @app.route('/api/topics/<topics_id>/permissions/list', methods=['GET'])
 @flask_login.login_required
@@ -18,20 +20,28 @@ def topic_permissions_list(topics_id):
     results = user_mc.topicPermissionsList(topics_id)
     return jsonify(results)
 
+
 @app.route('/api/topics/<topics_id>/permissions/update', methods=['PUT'])
-@form_fields_required('email', 'permission')
 @flask_login.login_required
 @api_error_handler
 def topic_update_permission(topics_id):
-    email = request.form["email"]
-    permission = request.form["permission"]
-    if permission not in ['read', 'write', 'admin', 'none']:
-        return json_error_response('Invalid permission value')
     user_mc = user_admin_mediacloud_client()
-    try:
-        results = user_mc.topicPermissionsUpdate(topics_id, email, permission)
-    except MCException as e:
-        # show a nice error if they type the email wrong
-        if 'Unknown email' in e.message:
-            return jsonify({'success': 0, 'results': e.message})
-    return jsonify({'success': 1, 'results': results})
+    new_permissions = json.loads(request.form["permissions"])
+    current_permissions = user_mc.topicPermissionsList(topics_id)['permissions']
+    # first remove any people that you need to
+    new_emails = [p['email'] for p in new_permissions]
+    current_emails = [p['email'] for p in current_permissions]
+    for email in current_emails:
+        if email not in new_emails:
+            user_mc.topicPermissionsUpdate(topics_id, email, 'none')
+    # now update the remaining permissions
+    for permission in new_permissions:
+        if permission['permission'] not in [u'read', u'write', u'admin', u'none']:
+            return json_error_response('Invalid permission value')
+        try:
+            user_mc.topicPermissionsUpdate(topics_id, permission['email'], permission['permission'])
+        except MCException as e:
+            # show a nice error if they type the email wrong
+            if 'Unknown email' in e.message:
+                return jsonify({'success': 0, 'results': e.message})
+    return jsonify({'success': 1, 'results': user_mc.topicPermissionsList(topics_id)})
--- src/components/topic/permissions/TopicPermissionsContainer.js
+++ src/components/topic/permissions/TopicPermissionsContainer.js
@@ -1,25 +1,76 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { reduxForm } from 'redux-form';
+import { Field, FieldArray, reduxForm } from 'redux-form';
 import { FormattedMessage, injectIntl } from 'react-intl';
+import MenuItem from '@material-ui/core/MenuItem';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
-import PermissionForm from './PermissionForm';
-import { fetchPermissionsList, updatePermission } from '../../../actions/topicActions';
+import { fetchPermissionsList, updatePermissions } from '../../../actions/topicActions';
 import withAsyncFetch from '../../common/hocs/AsyncContainer';
 import BackLinkingControlBar from '../BackLinkingControlBar';
 import { updateFeedback } from '../../../actions/appActions';
-import { PERMISSION_TOPIC_NONE } from '../../../lib/auth';
+import { PERMISSION_TOPIC_READ, PERMISSION_TOPIC_WRITE, PERMISSION_TOPIC_ADMIN } from '../../../lib/auth';
+import withIntlForm from '../../common/hocs/IntlForm';
 import messages from '../../../resources/messages';
+import AppButton from '../../common/AppButton';
+import { DeleteButton } from '../../common/IconButton';
+import { invalidEmail } from '../../../lib/formValidators';
 
 const localMessages = {
   title: { id: 'topic.permissions.title', defaultMessage: 'Topic Permissions' },
   intro: { id: 'topic.permissions.intro', defaultMessage: 'You can control who is allowed to see, and who is allowed to edit, this Topic. Enter another user\'s email in the field below, set whether they can read or edit the topic, and then click add. Read permission allows the given user to view all data within the topic. Write permission grants read permission and also allows the user to perform all operations on the topic -- including spidering, snapshotting, and merging — other editing permissions. Admin permission grants write permission and also allows all the user to edit the permissions for the topic.' },
+  read: { id: 'topic.permissions.read', defaultMessage: 'Read' },
+  write: { id: 'topic.permissions.write', defaultMessage: 'Write' },
+  admin: { id: 'topic.permissions.admin', defaultMessage: 'Admin' },
   existingTitle: { id: 'topic.permissions.existing.title', defaultMessage: 'Current Permissions' },
   existingIntro: { id: 'topic.permissions.existing.intro', defaultMessage: 'Here is a list of the current users and what they are allowed to do.' },
   addTitle: { id: 'topic.permissions.add', defaultMessage: 'Add Someone to this Topic' },
-  invalidEmail: { id: 'topic.permissions.email.invalid', defaultMessage: '⚠️ We don\'t recognize that email!' },
+  emailError: { id: 'topic.permissions.email.error', defaultMessage: 'You have to enter the email of a Media Cloud user.' },
+  failedToSave: { id: 'topic.permissions.email.failedSave', defaultMessage: 'Something went wrong! We couldn\'t save those permissions' },
+  saved: { id: 'topic.permissions.email.saveWorked', defaultMessage: 'Saved the new permissions.' },
+  emailFieldHint: { id: 'topic.permissions.email.hint', defaultMessage: 'Enter someone\'s email' },
 };
+
+
+// render a list of permissions and the option to add one more
+const PermissionsList = ({ renderTextField, renderSelect, intl: { formatMessage }, fields, meta: { error, submitFailed } }) => (
+  <div className="topic-permissions-list">
+    <AppButton onClick={() => fields.push({})}><FormattedMessage {...localMessages.addTitle} /></AppButton>
+    {submitFailed && error && <span>{error}</span>}
+    {fields.map((permission, index) => (
+      <div className="topic-permission-item" key={`permission${index}`}>
+        <Row>
+          <Col lg={5}>
+            <Field
+              name={`${permission}.email`}
+              component={renderTextField}
+              fullWidth
+              placeholder={formatMessage(localMessages.emailFieldHint)}
+            />
+          </Col>
+          <Col lg={3}>
+            <Field name={`${permission}.permission`} component={renderSelect} label={localMessages.permission}>
+              <MenuItem key={PERMISSION_TOPIC_READ} value={PERMISSION_TOPIC_READ}><FormattedMessage {...localMessages.read} /></MenuItem>
+              <MenuItem key={PERMISSION_TOPIC_WRITE} value={PERMISSION_TOPIC_WRITE}><FormattedMessage {...localMessages.write} /></MenuItem>
+              <MenuItem key={PERMISSION_TOPIC_ADMIN} value={PERMISSION_TOPIC_ADMIN}><FormattedMessage {...localMessages.admin} /></MenuItem>
+            </Field>
+            <DeleteButton onClick={() => fields.remove(index)} />
+          </Col>
+        </Row>
+      </div>
+    ))}
+  </div>
+);
+PermissionsList.propTypes = {
+  fields: PropTypes.object.isRequired,
+  meta: PropTypes.object.isRequired,
+  // from hoc
+  renderTextField: PropTypes.func.isRequired,
+  renderSelect: PropTypes.func.isRequired,
+  intl: PropTypes.object.isRequired,
+};
+const HocPermissionsList = injectIntl(withIntlForm(PermissionsList));
+
 
 class TopicPermissionsContainer extends React.Component {
   componentWillReceiveProps(nextProps) {
@@ -30,46 +81,36 @@ class TopicPermissionsContainer extends React.Component {
   }
 
   render() {
-    const { handleUpdate, permissions, topicId, handleSubmit, handleDelete } = this.props;
-    const permissionsPlusOne = [{ email: '', permission: '' }, ...permissions];
+    const { handleUpdate, topicId, handleSubmit, pristine, submitting } = this.props;
     return (
-      <div className="topic-permissioned">
+      <React.Fragment>
         <BackLinkingControlBar message={messages.backToTopic} linkTo={`/topics/${topicId}/summary`} />
-        <form name="updatePermissionFormParent" onSubmit={handleSubmit} onDelete={handleDelete}>
-          <div className="topic-acl">
-            <Grid>
-              <Row>
-                <Col lg={12} md={12} sm={12}>
-                  <h1><FormattedMessage {...localMessages.title} /></h1>
-                  <p><FormattedMessage {...localMessages.intro} /></p>
-                </Col>
-              </Row>
-              <Row>
-                <Col lg={12} md={12} sm={12}>
-                  <h2><FormattedMessage {...localMessages.addTitle} /></h2>
-                </Col>
-              </Row>
-              <PermissionForm index="0" initialValues={permissionsPlusOne} onSave={handleUpdate} />
-              <Row>
-                <Col md={10} sm={12}>
-                  <h2><FormattedMessage {...localMessages.existingTitle} /></h2>
-                  <p><FormattedMessage {...localMessages.existingIntro} /></p>
-                </Col>
-              </Row>
-              { permissions.map((p, index) => (
-                <PermissionForm
-                  index={`${index + 1}`}
-                  key={p.email}
-                  onSave={handleUpdate}
-                  initialValues={permissionsPlusOne} // b/c of how this is constructed - we need each form to have all the permission data
-                  showDeleteButton
-                  onDelete={handleDelete}
+        <Grid>
+          <Row>
+            <Col lg={10}>
+              <h1><FormattedMessage {...localMessages.title} /></h1>
+              <p><FormattedMessage {...localMessages.intro} /></p>
+            </Col>
+          </Row>
+          <form name="updatePermissionFormParent" onSubmit={handleSubmit(handleUpdate)}>
+            <Row>
+              <Col lg={12} md={12} sm={12}>
+                <FieldArray name="permissions" component={HocPermissionsList} />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={2}>
+                <AppButton
+                  type="submit"
+                  disabled={pristine || submitting}
+                  label={messages.save}
+                  primary
                 />
-              ))}
-            </Grid>
-          </div>
-        </form>
-      </div>
+              </Col>
+            </Row>
+          </form>
+        </Grid>
+      </React.Fragment>
     );
   }
 }
@@ -81,7 +122,6 @@ TopicPermissionsContainer.propTypes = {
   handleUpdate: PropTypes.func.isRequired,
   fetchData: PropTypes.func.isRequired,
   asyncFetch: PropTypes.func.isRequired,
-  handleDelete: PropTypes.func.isRequired,
   // from state
   topicId: PropTypes.number,
   fetchStatus: PropTypes.string.isRequired,
@@ -89,33 +129,30 @@ TopicPermissionsContainer.propTypes = {
   // from form helper
   initialValues: PropTypes.object,
   handleSubmit: PropTypes.func,
+  pristine: PropTypes.bool,
+  submitting: PropTypes.bool,
 };
 
 const mapStateToProps = state => ({
   topicId: state.topics.selected.id,
   fetchStatus: state.topics.selected.permissions.fetchStatus,
   permissions: state.topics.selected.permissions.list,
+  initialValues: { permissions: state.topics.selected.permissions.list },
   formValues: state.form.updatePermissionFormParent,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  doUpdatePermission: (topicId, values) => {
+  updatePermissions: (topicId, permissions) => {
     // save and then update the list of existing permissions
-    dispatch(updatePermission(topicId, values.email, values.permission))
+    dispatch(updatePermissions(topicId, permissions))
       .then((response) => {
         if (response.success === 0) {
-          dispatch(updateFeedback({
-            open: true,
-            message: ownProps.intl.formatMessage(localMessages.invalidEmail),
-          }));
+          dispatch(updateFeedback({ open: true, message: ownProps.intl.formatMessage(localMessages.failedToSave) }));
         } else {
+          dispatch(updateFeedback({ open: true, message: ownProps.intl.formatMessage(localMessages.saved) }));
           dispatch(fetchPermissionsList(topicId));
         }
       });
-  },
-  doDeletePermission: (topicId, values) => {
-    dispatch(updatePermission(topicId, values.email, PERMISSION_TOPIC_NONE))
-      .then(() => dispatch(fetchPermissionsList(topicId)));
   },
   fetchData: (topicId) => {
     dispatch(fetchPermissionsList(topicId));
@@ -124,22 +161,32 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
 
 function mergeProps(stateProps, dispatchProps, ownProps) {
   return Object.assign({}, stateProps, dispatchProps, ownProps, {
-    handleUpdate: (index) => {
-      dispatchProps.doUpdatePermission(stateProps.topicId, stateProps.formValues.values[index]);
-    },
-    handleDelete: (index) => {
-      dispatchProps.doDeletePermission(stateProps.topicId, stateProps.formValues.values[index]);
+    handleUpdate: (values) => {
+      dispatchProps.updatePermissions(stateProps.topicId, values.permissions);
     },
     asyncFetch: () => {
       dispatchProps.fetchData(stateProps.topicId);
     },
   });
 }
+
+function validate(values) {
+  const errors = {};
+  const permissionArrayErrors = [];
+  values.permissions.forEach((permission, index) => {
+    const permissionErrors = {};
+    if (invalidEmail(permission.email)) {
+      permissionErrors.email = localMessages.emailError;
+    }
+    permissionArrayErrors[index] = permissionErrors;
+  });
+  errors.permissions = permissionArrayErrors;
+  return errors;
+}
+
 const reduxFormConfig = {
   form: 'updatePermissionFormParent',
-  enableReinitialize: true,
-  // validate,
-  destroyOnUnmount: false,
+  validate,
 };
 
 export default

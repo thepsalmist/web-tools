ce5d06cb311850597e8447265b701da9acaf9adc
Rahul B <rahulbot@gmail.com>
2018-10-05 16:39:53 -0400

    More sources method fixes

--- server/views/sources/apicache.py
+++ server/views/sources/apicache.py
@@ -71,8 +71,12 @@ def last_year_split_story_count(user_mc_key, q='*'):
     return _cached_last_year_split_story_count(user_mc_key, q)
 
 
+def timeperiod_story_count(user_mc, query, time_period):
+    return _cached_timeperiod_story_count(query, time_period)
+
+
 @cache.cache_on_arguments(function_key_generator=key_generator)
-def cached_timeperiod_story_count(q='*', time_period=QUERY_LAST_MONTH):
+def _cached_timeperiod_story_count(q='*', time_period=QUERY_LAST_MONTH):
     # sources are open to everyone, so no need for user-specific cache
     # Helper to fetch split story counts over a timeframe for an arbitrary query
     user_mc = user_mediacloud_client()

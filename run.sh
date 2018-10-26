60f1296c3f2ab5a78dfdd1609193aa3e363a2158
Rahul B <rahulbot@gmail.com>
2018-10-26 11:44:36 -0400

    Switch gunicorn to use async works (for long downloads)

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-source venv/bin/activate
-
-# Workers might stall for a while on slower API requests
-WORKER_TIMEOUT=60
-
-gunicorn --timeout=$WORKER_TIMEOUT server:app
-
+gunicorn server:app -k gevent

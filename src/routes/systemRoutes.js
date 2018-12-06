db90687a7a2f82c6763ab422daab7add2b403b18
Rahul B <rahulbot@gmail.com>
2018-12-06 13:45:49 -0500

    centralize routes so you can visit admin menu from any tool #1368

--- src/routes/systemRoutes.js
+++ src/routes/systemRoutes.js
@@ -2,11 +2,27 @@ import React from 'react';
 import Route from 'react-router/lib/Route';
 import RecentNewsContainer from '../components/common/news/RecentNewsContainer';
 import PageNotFound from '../components/PageNotFound';
+import ManageUsersContainer from '../components/common/admin/ManageUsersContainer';
+import UpdateUserContainer from '../components/common/admin/UpdateUserContainer';
+import StoryDetailsContainer from '../components/common/admin/StoryDetailsContainer';
+import UpdateStoryContainer from '../components/common/admin/UpdateStoryContainer';
+import { requireAuth } from './routes';
 
 const systemRoutes = (
   <Route path="/">
 
     <Route path="/recent-news" component={RecentNewsContainer} />
+
+    <Route path="/admin">
+
+      <Route path="/admin/users/:id/update" component={UpdateUserContainer} onEnter={requireAuth} />
+      <Route path="/admin/users/list" component={ManageUsersContainer} onEnter={requireAuth} />
+
+      <Route path="/admin/story/details" component={StoryDetailsContainer} onEnter={requireAuth} />
+      <Route path="/admin/story/:id/details" component={StoryDetailsContainer} onEnter={requireAuth} />
+      <Route path="/admin/story/:id/update" component={UpdateStoryContainer} onEnter={requireAuth} />
+
+    </Route>
 
     <Route path="*" component={PageNotFound} />
 
--- src/routes/toolsRoutes.js
+++ src/routes/toolsRoutes.js
@@ -3,13 +3,8 @@ import Route from 'react-router/lib/Route';
 import IndexRedirect from 'react-router/lib/IndexRedirect';
 import ToolsApp from '../components/tools/ToolsApp';
 import ToolsHomeContainer from '../components/tools/ToolsHomeContainer';
-import ManageUsersContainer from '../components/common/admin/ManageUsersContainer';
-import UpdateUserContainer from '../components/common/admin/UpdateUserContainer';
-import StoryDetailsContainer from '../components/common/admin/StoryDetailsContainer';
-import UpdateStoryContainer from '../components/common/admin/UpdateStoryContainer';
 import userRoutes from './userRoutes';
 import systemRoutes from './systemRoutes';
-import { requireAuth } from './routes';
 
 const toolsRoutes = (
   <Route path="/" component={ToolsApp}>
@@ -17,13 +12,6 @@ const toolsRoutes = (
     <IndexRedirect to="/home" />
 
     <Route path="home" component={ToolsHomeContainer} />
-    <Route path="/admin">
-      <Route path="/admin/users/:id/update" component={UpdateUserContainer} onEnter={requireAuth} />
-      <Route path="/admin/users/list" component={ManageUsersContainer} onEnter={requireAuth} />
-      <Route path="/admin/story/details" component={StoryDetailsContainer} onEnter={requireAuth} />
-      <Route path="/admin/story/:id/details" component={StoryDetailsContainer} onEnter={requireAuth} />
-      <Route path="/admin/story/:id/update" component={UpdateStoryContainer} onEnter={requireAuth} />
-    </Route>
     {userRoutes}
     {systemRoutes}
 

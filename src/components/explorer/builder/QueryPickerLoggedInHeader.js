00253dd57f4ca71c2fc6f398c14030bc4652bb7b
Rahul B <rahulbot@gmail.com>
2019-01-10 15:49:10 -0500

    Optimize query picker menu code #1287

--- src/components/explorer/builder/QueryPickerLoggedInHeader.js
+++ src/components/explorer/builder/QueryPickerLoggedInHeader.js
@@ -4,10 +4,12 @@ import { FormattedMessage, injectIntl } from 'react-intl';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import ColorPicker from '../../common/ColorPicker';
 import { QUERY_LABEL_CHARACTER_LIMIT, ACTION_MENU_ITEM_CLASS } from '../../../lib/explorerUtil';
 import { defaultMenuOriginProps } from '../../util/uiUtil';
+import { trimToMaxLength } from '../../../lib/stringUtil';
 
 const localMessages = {
   rename: { id: 'explorer.querypicker.title', defaultMessage: 'Rename Query' },
@@ -31,69 +33,56 @@ class QueryPickerLoggedInHeader extends React.Component {
   render() {
     const { query, isDeletable, onColorChange, onDuplicate, onDelete, onLabelEditRequest } = this.props;
     let nameInfo = <div />;
-    let menuChildren = null;
-    let iconOptions = null;
+    // construct the menu to use for each
+    const menuChildren = [
+      <MenuItem key="edit" className={ACTION_MENU_ITEM_CLASS} onClick={() => { onLabelEditRequest(); this.handleClose(); }}>
+        <FormattedMessage {...localMessages.rename} />
+      </MenuItem>,
+      <MenuItem key="duplicate" className={ACTION_MENU_ITEM_CLASS} onClick={() => { onDuplicate(); this.handleClose(); }}>
+        <FormattedMessage {...localMessages.duplicate} />
+      </MenuItem>,
+      <MenuItem key="color" className="color-picker-menu-item">
+        <ColorPicker
+          name="color"
+          color={query.color}
+          onChange={(e) => { onColorChange(e.value); this.handleClose(); }}
+          showLabel
+        />
+      </MenuItem>,
+    ];
     if (isDeletable()) { // if this is not the only QueryPickerItem
-      menuChildren = (
-        <div>
-          <MenuItem className={ACTION_MENU_ITEM_CLASS} onClick={() => { onLabelEditRequest(); this.handleClose(); }}><FormattedMessage {...localMessages.rename} /></MenuItem>
-          <MenuItem className={ACTION_MENU_ITEM_CLASS} onClick={() => { onDuplicate(); this.handleClose(); }}><FormattedMessage {...localMessages.duplicate} /></MenuItem>
-          <MenuItem className={ACTION_MENU_ITEM_CLASS} onClick={() => { onDelete(query); this.handleClose(); }}><FormattedMessage {...localMessages.delete} /></MenuItem>
-        </div>
-      );
-    } else {
-      menuChildren = (
-        <div>
-          <MenuItem className={ACTION_MENU_ITEM_CLASS} onClick={() => { onLabelEditRequest(); this.handleClose(); }}><FormattedMessage {...localMessages.rename} /></MenuItem>
-          <MenuItem className={ACTION_MENU_ITEM_CLASS} onClick={() => { onDuplicate(); this.handleClose(); }}><FormattedMessage {...localMessages.duplicate} /></MenuItem>
-        </div>
-      );
+      menuChildren.push(<Divider />);
+      menuChildren.push(<MenuItem key="delete" className={ACTION_MENU_ITEM_CLASS} onClick={() => { onDelete(query); this.handleClose(); }}><FormattedMessage {...localMessages.delete} /></MenuItem>);
     }
-    if (menuChildren !== null) {
-      iconOptions = (
-        <div className="query-picker-icon-button">
-          <IconButton onClick={this.handleClick} aria-haspopup="true" aria-owns="logged-in-header-menu"><MoreVertIcon /></IconButton>
-          <Menu
-            id="logged-in-header-menu"
-            open={Boolean(this.state.anchorEl)}
-            className="query-picker-icon-button"
-            {...defaultMenuOriginProps}
-            anchorEl={this.state.anchorEl}
-            onBackdropClick={this.handleClose}
-            onClose={this.handleClose}
+    // build the menu
+    if (query) {
+      nameInfo = (
+        <div>
+          <ColorPicker
+            color={query.color}
+            onChange={(e) => { onColorChange(e.value); this.handleClose(); }}
+          />&nbsp;
+          <span
+            className="query-picker-name"
           >
-            {menuChildren}
-            <MenuItem className="color-picker-menu-item">
-              <ColorPicker
-                name="color"
-                color={query.color}
-                onChange={(e) => { onColorChange(e.value); this.handleClose(); }}
-                showLabel
-              />
-            </MenuItem>
-          </Menu>
+            {trimToMaxLength(query.label, QUERY_LABEL_CHARACTER_LIMIT)}
+          </span>
+          <div className="query-picker-icon-button">
+            <IconButton onClick={this.handleClick} aria-haspopup="true" aria-owns="logged-in-header-menu"><MoreVertIcon /></IconButton>
+            <Menu
+              id="logged-in-header-menu"
+              open={Boolean(this.state.anchorEl)}
+              className="query-picker-icon-button"
+              {...defaultMenuOriginProps}
+              anchorEl={this.state.anchorEl}
+              onBackdropClick={this.handleClose}
+              onClose={this.handleClose}
+            >
+              {menuChildren}
+            </Menu>
+          </div>
         </div>
       );
-      let abbrevQuery = query.label;
-      if (abbrevQuery.length > QUERY_LABEL_CHARACTER_LIMIT) {
-        abbrevQuery = abbrevQuery.slice(0, QUERY_LABEL_CHARACTER_LIMIT).concat('...');
-      }
-      if (query) {
-        nameInfo = (
-          <div>
-            <ColorPicker
-              color={query.color}
-              onChange={(e) => { onColorChange(e.value); this.handleClose(); }}
-            />&nbsp;
-            <span
-              className="query-picker-name"
-            >
-              {abbrevQuery}
-            </span>
-            {iconOptions}
-          </div>
-        );
-      }
     }
     return nameInfo;
   }

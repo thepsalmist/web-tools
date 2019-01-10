e8a51babbda3b615a512d69599cba1ab7ef11904
Rahul B <rahulbot@gmail.com>
2019-01-10 15:26:38 -0500

    more work to streamline combineFetchStatus and support string, array, object

--- src/lib/fetchConstants.js
+++ src/lib/fetchConstants.js
@@ -8,7 +8,7 @@ const missingFetchStatusError = {
   msg: 'fetchStatuses is undefined!',
 };
 
-export const combineFetchStatuses = (fetchStatusesArray) => {
+function combineArrayOfFetchStatuses(fetchStatusesArray) {
   if (fetchStatusesArray === undefined) {
     throw missingFetchStatusError;
   }
@@ -29,12 +29,35 @@ export const combineFetchStatuses = (fetchStatusesArray) => {
     return FETCH_SUCCEEDED;
   }
   return FETCH_ONGOING;
-};
+}
 
-export const combineIndexedFetchStatuses = (indexedFetchStatuses) => {
+function combineIndexedFetchStatuses(indexedFetchStatuses) {
   if (indexedFetchStatuses === undefined) {
     throw missingFetchStatusError;
   }
   const fetchStatusesArray = Object.keys(indexedFetchStatuses).map(k => indexedFetchStatuses[k]);
-  return combineFetchStatuses(fetchStatusesArray);
+  return combineArrayOfFetchStatuses(fetchStatusesArray);
+}
+
+// accept a string, an array, or an object
+export function combineFetchStatuses(input) {
+  let fetchStatusToUse;
+  if (input === undefined) {
+    throw missingFetchStatusError;
+  // support a basic string
+  } else if (typeof input === 'string') {
+    fetchStatusToUse = input;
+  // or an array of strings
+  } else if (Array.isArray(input)) {
+    fetchStatusToUse = combineArrayOfFetchStatuses(input);
+  // or an object keyed to strings
+  } else if (typeof input === 'object' && input !== null) {
+    fetchStatusToUse = combineIndexedFetchStatuses(input);
+  } else {
+    const badFetchStatusType = {
+      msg: `fetchStatus received is not a valid type - it is ${typeof input}`,
+    };
+    throw badFetchStatusType;
+  }
+  return fetchStatusToUse;
 }

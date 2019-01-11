cdc5d1afe3d0d3bfc4a5023f3242d0d0dbd25cf5
Rahul B <rahulbot@gmail.com>
2019-01-11 15:12:36 -0500

    update PagedContainer to allow a static onPageChange handler

--- src/components/common/hocs/PagedContainer.js
+++ src/components/common/hocs/PagedContainer.js
@@ -1,39 +1,69 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+// import Button from '@material-ui/core/Button';
+import AppButton from '../AppButton';
 import messages from '../../../resources/messages';
 
 /**
  * Use this with the JS Composition pattern to make a Container that has paging controls.
- * Your container MUST expose `previousPage` and `nextPage` event handler methods.
- * Most likely those methods will call out to a fetchData property that loads the previous or
- * next page.
- * Your container receives:
+ *
+ * Your container MUST expost a `links` property.
+ *
+ * Your container MUST do one of the following:
+ * (a) expose `previousPage` and `nextPage` event handler methods.
+ * (b) pass in a onPageChange handler function that accepts (dispatch, props, linkId)
+ *
+ * Your container receives two buttons:
  * - nextButton (if there is another page)
  * - prevButton (if there is previous page)
  */
-function withPaging(ComposedContainer) {
+function withPaging(ComposedContainer, onPageChange) {
   const PagedContainer = (props) => {
-    const { links, nextPage, previousPage } = props;
+    const { links, nextPage, previousPage, dispatch } = props;
     const { formatMessage } = props.intl;
     let previousButton = null;
     if ((links !== undefined) && {}.hasOwnProperty.call(links, 'previous')) {
-      previousButton = <Button variant="outlined" label={formatMessage(messages.previousPage)} color="primary" onClick={previousPage}><FormattedMessage {...messages.previousPage} /></Button>;
+      const prevHandler = onPageChange || previousPage;
+      previousButton = (
+        <div className="paging-button paging-button-previous">
+          <AppButton
+            label={formatMessage(messages.previousPage)}
+            onClick={() => prevHandler(dispatch, props, links.previous)}
+          >
+            <FormattedMessage {...messages.previousPage} />
+          </AppButton>
+        </div>
+      );
     }
     let nextButton = null;
     if ((links !== undefined) && {}.hasOwnProperty.call(links, 'next')) {
-      nextButton = <Button variant="contained" label={formatMessage(messages.nextPage)} color="primary" onClick={nextPage}><FormattedMessage {...messages.nextPage} /></Button>;
+      const nextHandler = onPageChange || nextPage;
+      nextButton = (
+        <div className="paging-button paging-button-previous">
+          <AppButton
+            primary
+            label={formatMessage(messages.nextPage)}
+            onClick={() => nextHandler(dispatch, props, links.next)}
+          >
+            <FormattedMessage {...messages.nextPage} />
+          </AppButton>
+        </div>
+      );
     }
     return <ComposedContainer {...props} nextButton={nextButton} previousButton={previousButton} />;
   };
   PagedContainer.propTypes = {
+    // from compositional chain
     intl: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    // from child
     links: PropTypes.object.isRequired,
-    nextPage: PropTypes.func.isRequired,
-    previousPage: PropTypes.func.isRequired,
+    nextPage: PropTypes.func,
+    previousPage: PropTypes.func,
   };
-  return PagedContainer;
+  return connect()(PagedContainer);
 }
 
 export default withPaging;

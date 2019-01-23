725e03aef2280edd4a97bcc347fcee492f83b12e
Rahul B <rahulbot@gmail.com>
2019-01-23 17:07:28 -0500

    Refactor topic media search to use new autocomplete widget #1448

--- src/components/topic/search/TopicSourceSearchContainer.js
+++ src/components/topic/search/TopicSourceSearchContainer.js
@@ -5,7 +5,6 @@ import { injectIntl } from 'react-intl';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
 import { push } from 'react-router-redux';
 import SimpleSourceSearchContainer from '../../common/SimpleSourceSearchContainer';
-import { urlToTopicMapper } from '../../../lib/urlUtil';
 
 const TopicSourceSearchContainer = props => (
   <div className="controlbar controlbar-sources">
@@ -16,10 +15,7 @@ const TopicSourceSearchContainer = props => (
           <Col lg={6} xs={12} className="right">
             {(props.showSearch === true) && (
               <SimpleSourceSearchContainer
-                searchSources
-                maxSources={12}
                 onMediaSourceSelected={props.handleMediaSourceSelected}
-                goToThisUrl={mediaItem => props.handleUrlCallback(props.topicId, mediaItem)}
               />
             )}
           </Col>
@@ -38,27 +34,13 @@ TopicSourceSearchContainer.propTypes = {
   intl: PropTypes.object.isRequired,
   // from dispatch
   handleMediaSourceSelected: PropTypes.func.isRequired,
-  handleCollectionSelected: PropTypes.func.isRequired,
-  handleAdvancedSearchSelected: PropTypes.func.isRequired,
-  handleUrlCallback: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = () => ({
 });
 
-const mapDispatchToProps = dispatch => ({
-  handleMediaSourceSelected: (item) => {
-    dispatch(push(`/media/${item.id}`));
-  },
-  handleCollectionSelected: (item) => {
-    dispatch(push(`/media/${item.id}`));
-  },
-  handleAdvancedSearchSelected: (item) => {
-    dispatch(push(`/media/${item.id}`));
-  },
-  handleUrlCallback: (topicId, mediaItem) => {
-    window.location = urlToTopicMapper(`topics/${topicId}/media/${mediaItem}`);
-  }, // this may or may not work depending if the source is within the topic
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  handleMediaSourceSelected: mediaId => dispatch(push(`topics/${ownProps.topicId}/media/${mediaId}`)),
 });
 
 export default

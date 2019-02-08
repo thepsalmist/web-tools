04ff9056dee2701321a993d0c2b01e13a03a0a04
Rahul B <rahulbot@gmail.com>
2019-02-08 09:18:49 -0500

    Fix missing import parse error

--- src/components/explorer/results/QueryAttentionOverTimeResultsContainer.js
+++ src/components/explorer/results/QueryAttentionOverTimeResultsContainer.js
@@ -16,7 +16,7 @@ import AttentionOverTimeChart, { dataAsSeries } from '../../vis/AttentionOverTim
 import { DownloadButton } from '../../common/IconButton';
 import ActionMenu from '../../common/ActionMenu';
 import { oneDayLater, solrFormat } from '../../../lib/dateUtil';
-import { postToDownloadUrl, ACTION_MENU_ITEM_CLASS, ensureSafeResults, formatQueryForServer, formatDemoQueryForServer } from '../../../lib/explorerUtil';
+import { postToDownloadUrl, postToCombinedDownloadUrl, ACTION_MENU_ITEM_CLASS, ensureSafeResults, formatQueryForServer, formatDemoQueryForServer } from '../../../lib/explorerUtil';
 import messages from '../../../resources/messages';
 import { FETCH_INVALID } from '../../../lib/fetchConstants';
 

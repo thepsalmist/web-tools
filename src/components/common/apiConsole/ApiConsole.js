0ac59079dea83eff07e63bb6063005218c713288
Rahul B <rahulbot@gmail.com>
2019-03-08 13:19:18 -0500

    API console tweaks #1417

--- src/components/common/apiConsole/ApiConsole.js
+++ src/components/common/apiConsole/ApiConsole.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
 import { FormattedMessage, FormattedHTMLMessage, injectIntl } from 'react-intl';
 import { Helmet } from 'react-helmet';
@@ -6,19 +7,21 @@ import SwaggerContainer from './SwaggerContainer';
 
 const localMessages = {
   pageTitle: { id: 'apiConsole.pageTitle', defaultMessage: 'API Console' },
-  title: { id: 'apiConsole.title', defaultMessage: 'Try Out the Media Cloud API' },
-  intro: { id: 'apiConsole.intro', defaultMessage: 'All of our data is available programatically via our API. If you are a coder, you can fetch data directly from our system using a variety of endpoints.  The console below lets you browse and try out many of our most useful API endpoints.  If you are planning to go this route and work in the Python programming language, do check out our <a target=_new href="https://pypi.org/project/mediacloud/">Python API CLient</a>.' },
+  subTitle: { id: 'apiConsole.subTitle', defaultMessage: 'Try Out the Media Cloud API' },
+  intro: { id: 'apiConsole.intro', defaultMessage: 'All of our data is available programatically via our API. If you are a coder, you can fetch data directly from our system using a variety of endpoints.  The console below lets you browse and try out some of our most commonly used API endpoints.  If you are planning to go this route and work in the Python programming language, read more:<ul><li><a target=_new href="https://github.com/berkmancenter/mediacloud/blob/master/doc/api_2_0_spec/api_2_0_spec.md">Full API Documentation</a></li><li><a target=_new href="https://pypi.org/project/mediacloud/">Python API CLient</a></li></ul>' },
 };
 
-const ApiConsole = (props) => (
+const ApiConsole = props => (
   <Grid>
     <Helmet><title>{props.intl.formatMessage(localMessages.pageTitle)}</title></Helmet>
     <Row>
       <Col lg={8}>
-        <h1><FormattedMessage {...localMessages.title} /></h1>
+        <h1><FormattedMessage {...localMessages.pageTitle} /></h1>
+        <h2><FormattedMessage {...localMessages.subTitle} /></h2>
         <p><FormattedHTMLMessage {...localMessages.intro} /></p>
       </Col>
     </Row>
+    <hr />
     <Row>
       <Col lg={12}>
         <SwaggerContainer specUrl="/static/api-spec.json" />
@@ -26,5 +29,10 @@ const ApiConsole = (props) => (
     </Row>
   </Grid>
 );
+
+ApiConsole.propTypes = {
+  // from compositional chain
+  intl: PropTypes.object.isRequired,
+};
 
 export default injectIntl(ApiConsole);

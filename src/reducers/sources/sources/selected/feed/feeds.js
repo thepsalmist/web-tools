8703201d8f5325d58b699854584faf459ccbfcd2
Rahul B <rahulbot@gmail.com>
2019-03-28 16:32:33 -0400

    sort feeds by newest story download first #1503

--- src/reducers/sources/sources/selected/feed/feeds.js
+++ src/reducers/sources/sources/selected/feed/feeds.js
@@ -1,6 +1,6 @@
 import { createAsyncReducer } from '../../../../../lib/reduxHelpers';
 import { FETCH_SOURCE_FEEDS } from '../../../../../actions/sourceActions';
-
+import { postgresDateToMoment } from '../../../../../lib/dateUtil';
 
 const feeds = createAsyncReducer({
   initialState: {
@@ -10,7 +10,12 @@ const feeds = createAsyncReducer({
   action: FETCH_SOURCE_FEEDS,
   handleSuccess: payload => ({
     count: payload.count,
-    list: payload.results,
+    list: payload.results.map(feed => ({
+      ...feed,
+      lastDownloadMoment: feed.last_attempted_download_time ? postgresDateToMoment(feed.last_attempted_download_time) : null,
+      lastNewStoryMoment: feed.last_new_story_time ? postgresDateToMoment(feed.last_new_story_time) : null,
+      lastSuccessfulDownloadMoment: feed.last_successful_download_time ? postgresDateToMoment(feed.last_successful_download_time) : null,
+    })),
   }),
 });
 export default feeds;

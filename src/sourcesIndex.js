00fcdd22784bb754b7c7a45d0cfbe49627bc3fcc
Rahul B <rahulbot@gmail.com>
2019-05-03 13:52:53 -0400

    version bump for release

--- src/sourcesIndex.js
+++ src/sourcesIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-8');
 
-setVersion('3.9.1');
+setVersion('3.10.0');
 
 setAppName(APP_SOURCE_MANAGER);
 

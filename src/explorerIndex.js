d8c844b29c89385eb60114586466ed90f7e85ca7
Rahul B <rahulbot@gmail.com>
2019-05-24 09:03:54 -0400

    Release prep

--- src/explorerIndex.js
+++ src/explorerIndex.js
@@ -11,7 +11,7 @@ import routes from './routes/explorerRoutes';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-11');
 
-setVersion('3.10.6');
+setVersion('3.10.8');
 
 setAppName(APP_EXPLORER);
 
--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-7');
 
-setVersion('3.10.7');
+setVersion('3.10.8');
 
 setAppName(APP_TOPIC_MAPPER);
 

6cacbe9f5b412ccbc2037dce610037897d1025ff
cindyb <cindyloo_home at yahoo.com>
2019-06-06 14:43:33 -0400

    updated builds for v3.10.10

--- src/explorerIndex.js
+++ src/explorerIndex.js
@@ -11,7 +11,7 @@ import routes from './routes/explorerRoutes';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-11');
 
-setVersion('3.10.9');
+setVersion('3.10.10');
 
 setAppName(APP_EXPLORER);
 

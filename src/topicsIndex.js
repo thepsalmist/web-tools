392b4b5f61b706fba71543379979c75db1575760
cindyb <cindyloo_home at yahoo.com>
2019-06-27 16:14:39 -0400

    prep 3.11.0

--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-7');
 
-setVersion('3.10.10.1');
+setVersion('3.11.0');
 
 setAppName(APP_TOPIC_MAPPER);
 

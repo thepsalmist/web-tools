792f0c5a66b4f1408531b5ca1b4ed475cbcf7e32
cindyb <cindyloo_home at yahoo.com>
2019-06-27 17:14:54 -0400

    update to trigger push

--- src/explorerIndex.js
+++ src/explorerIndex.js
@@ -11,7 +11,7 @@ import routes from './routes/explorerRoutes';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-11');
 
-setVersion('3.11.0');
+setVersion('3.11.1');
 
 setAppName(APP_EXPLORER);
 

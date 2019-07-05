ebaef3b51be6fe318644f50e10c1f8b91c0a9633
Rahul B <rahulbot@gmail.com>
2019-07-05 13:47:08 -0400

    Version bump and build for topics release

--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-7');
 
-setVersion('3.11.1');
+setVersion('3.11.2');
 
 setAppName(APP_TOPIC_MAPPER);
 

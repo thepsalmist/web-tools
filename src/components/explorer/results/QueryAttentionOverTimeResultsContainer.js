6b92a4bfa9ab700da9068a5a0ce589a78f35d178
cindyb <cindyloo_home at yahoo.com>
2019-08-15 09:28:46 -0400

    #1635 drilldowns need searches as part of query!

--- src/components/explorer/results/QueryAttentionOverTimeResultsContainer.js
+++ src/components/explorer/results/QueryAttentionOverTimeResultsContainer.js
@@ -15,7 +15,7 @@ import AttentionOverTimeChart, { dataAsSeries } from '../../vis/AttentionOverTim
 import { DownloadButton } from '../../common/IconButton';
 import ActionMenu from '../../common/ActionMenu';
 import { oneDayLater, solrFormat } from '../../../lib/dateUtil';
-import { postToDownloadUrl, postToCombinedDownloadUrl, ACTION_MENU_ITEM_CLASS, ensureSafeResults } from '../../../lib/explorerUtil';
+import { postToDownloadUrl, postToCombinedDownloadUrl, ACTION_MENU_ITEM_CLASS, ensureSafeResults, prepSearches } from '../../../lib/explorerUtil';
 import messages from '../../../resources/messages';
 import { FETCH_INVALID } from '../../../lib/fetchConstants';
 
@@ -56,6 +56,7 @@ class QueryAttentionOverTimeResultsContainer extends React.Component {
         dayGap,
         sources: currentQueryOfInterest.sources.map(s => s.media_id),
         collections: currentQueryOfInterest.collections.map(c => c.tags_id),
+        searches: prepSearches(selectedQuery.searches), // for each query, go prep searches
       };
       clickedQuery.end_date = solrFormat(oneDayLater(date1), true);
       selectDataPoint(clickedQuery);

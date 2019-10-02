6478a08626318c71a3502d85beb954245f1747c0
cindyb <cindyloo_home at yahoo.com>
2019-10-02 08:47:37 -0400

    prep for explorer release

--- src/explorerIndex.js
+++ src/explorerIndex.js
@@ -11,7 +11,7 @@ import routes from './routes/explorerRoutes';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-11');
 
-setVersion('3.12.4');
+setVersion('3.12.5');
 
 setAppName(APP_EXPLORER);
 

05701f11f5980cf9844535786a2049790893d703
cindyb <cindyloo_home at yahoo.com>
2019-10-28 16:35:20 -0400

    wizard step 3

--- server/views/topics/platforms/openweb.py
+++ server/views/topics/platforms/openweb.py
@@ -19,35 +19,35 @@ PLATFORM_OPEN_WEB = '*'  # how are we distinguishing platform ids etc? How does 
 @api_error_handler
 def open_web_story_counts(topics_id):
     tag_story_counts = []
-    media_type_tags = tags_in_tag_set(TOOL_API_KEY, PLATFORM_OPEN_WEB)
+ #   media_type_tags = tags_in_tag_set(TOOL_API_KEY, PLATFORM_OPEN_WEB)
     # grab the total stories
-    total_stories = topic_story_count(user_mediacloud_key(), topics_id)['count']
+    #total_stories = topic_story_count(user_mediacloud_key(), topics_id)['count']
     # make a count for each tag based on media_id
-    for tag in media_type_tags:
-        query_clause = "tags_id_media:{}".format(tag['tags_id'])
-        tagged_story_count = topic_story_count(user_mediacloud_key(), topics_id, q=query_clause)['count']
-        tag_story_counts.append({
-            'label': tag['label'],
-            'tags_id': tag['tags_id'],
-            'count': tagged_story_count,
-            'pct': float(tagged_story_count)/float(total_stories) if total_stories > 0 else 0, # protect against div by zero
-        })
+    #for tag in media_type_tags:
+    #    query_clause = "tags_id_media:{}".format(tag['tags_id'])
+    #    tagged_story_count = topic_story_count(user_mediacloud_key(), topics_id, q=query_clause)['count']
+    #    tag_story_counts.append({
+    #        'label': tag['label'],
+    #        'tags_id': tag['tags_id'],
+    #        'count': tagged_story_count,
+    #        'pct': float(tagged_story_count)/float(total_stories) if total_stories > 0 else 0, # protect against div by zero
+    #    })
 
-    return jsonify({'story_counts': tag_story_counts})
+    return jsonify({'story_counts': {'label': 'open web', 'tags_id': 99999, 'count': 1, 'pct': 100}})
 
 
 @app.route('/api/topics/<topics_id>/platforms/open-web/coverage', methods=['GET'])
 @flask_login.login_required
 @api_error_handler
 def open_web_coverage(topics_id):
-    media_type_tags = tags_in_tag_set(TOOL_API_KEY, PLATFORM_OPEN_WEB)
+    #media_type_tags = tags_in_tag_set(TOOL_API_KEY, PLATFORM_OPEN_WEB)
     # grab the total stories
-    total_stories = topic_story_count(user_mediacloud_key(), topics_id)['count']
+    #total_stories = topic_story_count(user_mediacloud_key(), topics_id)['count']
     # count the stories in any media in tagged as media_type
-    tags_ids = " ".join(str(tag['tags_id']) for tag in media_type_tags)
-    query_clause = "tags_id_media:({})".format(tags_ids)
-    tagged_story_count = topic_story_count(user_mediacloud_key(), topics_id, q=query_clause)['count']
-    return jsonify({'counts': {'count': tagged_story_count, 'total': total_stories}})
+    #tags_ids = " ".join(str(tag['tags_id']) for tag in media_type_tags)
+    #query_clause = "tags_id_media:({})".format(tags_ids)
+    #tagged_story_count = topic_story_count(user_mediacloud_key(), topics_id, q=query_clause)['count']
+    return jsonify({'counts': {'count': 1, 'total': 1}})
 
 
 @app.route('/api/topics/<topics_id>/platforms/open-web/create', methods=['POST'])

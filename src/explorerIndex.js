7f79603241a75fa83a918ccc797c6d67341c430e
Rahul B <rahulbot@gmail.com>
2019-11-06 13:17:29 -0500

    Release prep

--- src/explorerIndex.js
+++ src/explorerIndex.js
@@ -11,7 +11,7 @@ import routes from './routes/explorerRoutes';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-11');
 
-setVersion('3.12.9');
+setVersion('3.12.10');
 
 setAppName(APP_EXPLORER);
 
--- src/sourcesIndex.js
+++ src/sourcesIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-8');
 
-setVersion('3.12.9');
+setVersion('3.12.10');
 
 setAppName(APP_SOURCE_MANAGER);
 

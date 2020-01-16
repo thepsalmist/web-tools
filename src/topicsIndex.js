940df59daaf363aec7f423e387a673e58c35b5e6
cindyb <cindyloo_home at yahoo.com>
2020-01-16 13:59:50 -0500

    prep for 3.13.1 release

--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-7');
 
-setVersion('3.13.0');
+setVersion('3.13.1');
 
 setAppName(APP_TOPIC_MAPPER);
 

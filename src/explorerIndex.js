21472fdae410a15619129a52d1372832a84cc5e3
Rahul B <rahulbot@gmail.com>
2020-01-24 09:31:33 -0500

    Prep and build for explorer release

--- src/explorerIndex.js
+++ src/explorerIndex.js
@@ -11,7 +11,7 @@ import routes from './routes/explorerRoutes';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-11');
 
-setVersion('3.13.3');
+setVersion('3.13.5');
 
 setAppName(APP_EXPLORER);
 

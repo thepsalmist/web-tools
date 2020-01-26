db7e554128e1a1e1e7680522bfb133d5789f9a2f
Rahul B <rahulbot@gmail.com>
2020-01-26 11:43:43 -0500

    Rename seed query editing workflow, because it is about only dates now (seed query moved to platform editing)

--- src/components/topic/wizard/EditWizardTopicContainer.js
+++ src/components/topic/wizard/EditWizardTopicContainer.js
@@ -7,15 +7,15 @@ import PageTitle from '../../common/PageTitle';
 import { TOPIC_FORM_MODE_EDIT } from './TopicForm';
 
 const localMessages = {
-  introTitle: { id: 'topic.modify.pageTitle', defaultMessage: 'Change Topic Seed Query: Configure' },
+  introTitle: { id: 'topic.modify.pageTitle', defaultMessage: 'Change Topic Dates / Spidering: Configure' },
   introDesc: { id: 'topic.modify.pageDesc', defaultMessage: 'Updating these fields will create a new version of your topic.' },
-  previewTitle: { id: 'topic.modify.preview.title', defaultMessage: 'Change Topic Seed Query: Preview' },
+  previewTitle: { id: 'topic.modify.preview.title', defaultMessage: 'Change Topic Dates / Spidering: Preview' },
   previewDesc: { id: 'topic.modify.preview.about',
     defaultMessage: '<b>Make sure your topic looks right before you create a new version</b>. We start your version by finding all the stories in our database that match your query. From there we follow all the links and download them. We check if they match your keywords, and if they do then we add them to your topic (this is called "spidering"). Check the result below and make sure your topic is finding you the stories you want before creating it.' },
-  validateTitle: { id: 'topic.modify.validate.title', defaultMessage: 'Change Topic Seed Query: Validate 30 Sample Stories' },
+  validateTitle: { id: 'topic.modify.validate.title', defaultMessage: 'Change Topic Dates / Spidering: Validate 30 Sample Stories' },
   validateDesc: { id: 'topic.modify.validate.about',
     defaultMessage: 'To make sure the stories that match your seed query are relevant to your research, you need to review these samples to see if these are the kinds of stories you want. For each story, click "yes" if it is about the topic you are interested in.  Click "no" if it is not about the topic you are intereseted in.' },
-  confirmTitle: { id: 'topic.modify.confirm.title', defaultMessage: 'Change Topic Seed Query: Review and Confirm' },
+  confirmTitle: { id: 'topic.modify.confirm.title', defaultMessage: 'Change Topic Dates / Spidering: Review and Confirm' },
   name: { id: 'topic.modify.confirm.name', defaultMessage: 'Name' },
   confirmDesc: { id: 'topic.modify.confirm.description', defaultMessage: 'Description' },
   confirmFeedback: { id: 'topic.modify.failed', defaultMessage: 'Successfully created a new version of your new topic!' },

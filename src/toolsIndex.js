06216d24ac3739f448ca4640feb8d985c85041f1
Rahul B <rahulbot@gmail.com>
2020-02-25 09:21:20 -0500

    Build for release

--- src/toolsIndex.js
+++ src/toolsIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-9');
 
-setVersion('3.13.0');
+setVersion('3.13.8');
 
 setAppName(APP_TOOLS);
 
--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-7');
 
-setVersion('3.13.6');
+setVersion('3.13.8');
 
 setAppName(APP_TOPIC_MAPPER);
 

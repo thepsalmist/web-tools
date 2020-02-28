5625adc8ed0a3efb62c4b917c69d74ecdbe07157
Rahul B <rahulbot@gmail.com>
2020-02-28 16:35:14 -0500

    Rebuild for topics release

--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-7');
 
-setVersion('3.13.8');
+setVersion('3.13.9');
 
 setAppName(APP_TOPIC_MAPPER);
 

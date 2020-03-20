12eea9472bd794c9656f0caef354cb4df76aebeb
Rahul B <rahulbot@gmail.com>
2020-03-19 21:03:25 -0400

    Rebuild for alpha release testing

--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-7');
 
-setVersion('3.14.0-alpha2');
+setVersion('3.14.0-alpha3');
 
 setAppName(APP_TOPIC_MAPPER);
 

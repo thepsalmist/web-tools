75bfafe1250c75149a5babf16a6b8b81d17e297c
Rahul B <rahulbot@gmail.com>
2020-03-26 10:34:06 -0400

    Build for topics release

--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -11,7 +11,7 @@ import initializeApp from './index';
 
 if (!doNotTrack(false)) ReactGA.initialize('UA-60744513-7');
 
-setVersion('3.13.11');
+setVersion('3.13.12');
 
 setAppName(APP_TOPIC_MAPPER);
 

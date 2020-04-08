ad523d3b143c5544a2c9764731bef8cc1e68c675
Rahul B <rahulbot@gmail.com>
2020-04-08 14:08:03 -0400

    Use the whole monitor dates for CH platform preview #1789

--- server/platforms/twitter_crimson_hexagon.py
+++ server/platforms/twitter_crimson_hexagon.py
@@ -21,9 +21,9 @@ class TwitterCrimsonHexagonProvider(ContentProvider):
     def count(self, query: str, start_date: dt.datetime, end_date: dt.datetime, **kwargs) -> int:
         """
         The number of tweets in the  monitor.
-        :param query:
-        :param start_date:
-        :param end_date:
+        :param query: ignored
+        :param start_date: ignored in favor of the monitor's start/end dates
+        :param end_date: ignored in favor of the monitor's start/end dates
         :param kwargs: monitor_id
         :return:
         """
@@ -31,15 +31,16 @@ class TwitterCrimsonHexagonProvider(ContentProvider):
             'id': kwargs['monitor_id'],
             'groupBy': 'DAILY',
         }
-        results = self._cached_query2('volume', start_date, end_date, params)
+        monitor_start, monitor_end = self._monitor_dates(kwargs['monitor_id'])
+        results = self._cached_query('volume', monitor_start, monitor_end, params)
         return results['numberOfDocuments']
 
     def count_over_time(self, query: str, start_date: dt.datetime, end_date: dt.datetime, **kwargs) -> Dict:
         """
         The number of tweets in the  monitor, broken out by day.
-        :param query:
-        :param start_date:
-        :param end_date:
+        :param query: ignored
+        :param start_date: ignored in favor of the monitor's start/end dates
+        :param end_date: ignored in favor of the monitor's start/end dates
         :param kwargs: monitor_id
         :return:
         """
@@ -47,7 +48,8 @@ class TwitterCrimsonHexagonProvider(ContentProvider):
             'id': kwargs['monitor_id'],
             'groupBy': 'DAILY',
         }
-        results = self._cached_query2('volume', start_date, end_date, params)
+        monitor_start, monitor_end = self._monitor_dates(kwargs['monitor_id'])
+        results = self._cached_query('volume', monitor_start, monitor_end, params)
         data = []
         for d in results['volume']:
             item_start_date = dt.datetime.strptime(d['startDate'][:10], CRIMSON_HEXAGON_DATE_FORMAT)
@@ -61,9 +63,9 @@ class TwitterCrimsonHexagonProvider(ContentProvider):
     def words(self, query: str, start_date: dt.datetime, end_date: dt.datetime, limit: int = 100, **kwargs) -> List[Dict]:
         """
         The top words in tweets in the monitor.
-        :param query:
-        :param start_date:
-        :param end_date:
+        :param query: ignored
+        :param start_date: ignored in favor of the monitor's start/end dates
+        :param end_date: ignored in favor of the monitor's start/end dates
         :param limit:
         :param kwargs: monitor_id
         :return:
@@ -71,7 +73,8 @@ class TwitterCrimsonHexagonProvider(ContentProvider):
         params = {
             'id': kwargs['monitor_id'],
         }
-        results = self._cached_query2('wordcloud', start_date, end_date, params)
+        monitor_start, monitor_end = self._monitor_dates(kwargs['monitor_id'])
+        results = self._cached_query('wordcloud', monitor_start, monitor_end, params)
         data = []
         for term, freq in results['data'].items():
             data.append({
@@ -83,18 +86,42 @@ class TwitterCrimsonHexagonProvider(ContentProvider):
         return data[:limit]
 
     @cache.cache_on_arguments()
-    def _cached_query2(self, url, start_date: dt.datetime, end_date: dt.datetime, params: Dict) -> Dict:
+    def _monitor_details(self, monitor_id) -> Dict:
+        """
+        Fetch the basic metadata about the monior the user wants to import from.
+        :param monitor_id:
+        :return:
+        """
+        monitor = self._cached_query('detail', None, None, {'id': monitor_id})
+        return monitor
+
+    def _monitor_dates(self, monitor_id) -> (str, str):
+        """
+        We need to use the monitor dates for all the preview information, because if we pass in the topic
+        dates then their API throws an error saying there is a mismatch. This can happen when a topic has a larger
+        date range than the monitor did. We are OK to use their dates because on the back-end we just import *all* the
+        content in the monitor (it doesn't filter by the dates of the topic).
+        :param monitor_id:
+        :return:
+        """
+        monitor = self._monitor_details(monitor_id)
+        return monitor['resultsStart'], monitor['resultsEnd']
+
+    @cache.cache_on_arguments()
+    def _cached_query(self, url, start_date: str, end_date: str, params: Dict) -> Dict:
         """
         :param url:
-        :param start_date:
-        :param end_date:
-        :param params:
+        :param start_date: the monitor's start date
+        :param end_date: the monitor's end date
+        :param params: should one item, called 'id' that is the monitor's id
         :return:
         """
         headers = {'Content-type': 'application/json'}
         params['auth'] = self._api_key
-        params['start'] = start_date.strftime(CRIMSON_HEXAGON_DATE_FORMAT)[:10]
-        params['end'] = end_date.strftime(CRIMSON_HEXAGON_DATE_FORMAT)[:10]
+        if start_date is not None:
+            params['start'] = start_date
+        if end_date is not None:
+            params['end'] = end_date
         r = requests.get(CRIMSON_HEXAGON_BASE_URL+url, headers=headers, params=params)
         return r.json()
 

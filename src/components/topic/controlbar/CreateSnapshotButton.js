d2f213b65ccb75fb09d69bcb2710738096244777
Dennis Jen <dsjen@media.mit.edu>
2020-05-21 13:04:18 -0400

    Fix misspelling

--- src/components/topic/controlbar/CreateSnapshotButton.js
+++ src/components/topic/controlbar/CreateSnapshotButton.js
@@ -9,7 +9,7 @@ const localMessages = {
   createSnapshot: { id: 'snapshot.create', defaultMessage: 'Create a new snapshot' },
   helpTitle: { id: 'snapshot.help.title', defaultMessage: 'About Snapshots' },
   helpText: { id: 'snapshot.help.text',
-    defaultMessage: '<p>A Snapshot is a fronzen-in-time collection of all the content in your Topic.  You can\'t change anything within a Snapshot; you can just browse and explore it.  If you want to make any changes you need to generate a new Snapshot.  They are frozen-in-time so you can use them for reproduceable research; you wouldn\'t want your changing out from under you while you are analyzing it, or once you have published a report.</p><p>Click the arrow to see a popup list of the other Snapshots within this topic.  Click one from the list that appears to switch to it.</p>',
+    defaultMessage: '<p>A Snapshot is a frozen-in-time collection of all the content in your Topic.  You can\'t change anything within a Snapshot; you can just browse and explore it.  If you want to make any changes you need to generate a new Snapshot.  They are frozen-in-time so you can use them for reproduceable research; you wouldn\'t want your changing out from under you while you are analyzing it, or once you have published a report.</p><p>Click the arrow to see a popup list of the other Snapshots within this topic.  Click one from the list that appears to switch to it.</p>',
   },
 };
 

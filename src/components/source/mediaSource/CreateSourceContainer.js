6b19c2924460bba7546d492cd9a73d679aa66d7d
Rahul B <rahulbot@gmail.com>
2020-10-01 11:38:24 -0400

    fix use of state in `mapStateToProps`
    Weird that the linting didn't catch these errots 🤔. I also took opportunity to make the React component cleaner.

--- src/components/source/mediaSource/CreateSourceContainer.js
+++ src/components/source/mediaSource/CreateSourceContainer.js
@@ -21,29 +21,25 @@ const localMessages = {
   saveFailed: { id: 'source.add.feedback.error', defaultMessage: 'Sorry, your request failed for some reason' },
 };
 
-const CreateSourceContainer = (props) => {
-  const { handleSave } = props;
-  const { formatMessage } = props.intl;
-  return (
-    <div className="create-source">
-      <Permissioned onlyRole={PERMISSION_MEDIA_EDIT}>
-        <PageTitle value={localMessages.mainTitle} />
-        <Grid>
-          <Row>
-            <Col lg={12}>
-              <h1><FormattedMessage {...localMessages.mainTitle} /></h1>
-            </Col>
-          </Row>
-          <SourceForm
-            buttonLabel={formatMessage(localMessages.addButton)}
-            onSave={handleSave}
-            mediaMetadataSetsByName={mediaMetadataSetsByName}
-          />
-        </Grid>
-      </Permissioned>
-    </div>
-  );
-};
+const CreateSourceContainer = ({handleSave, mediaMetadataSetsByName, intl}) => (
+  <div className="create-source">
+    <Permissioned onlyRole={PERMISSION_MEDIA_EDIT}>
+      <PageTitle value={localMessages.mainTitle} />
+      <Grid>
+        <Row>
+          <Col lg={12}>
+            <h1><FormattedMessage {...localMessages.mainTitle} /></h1>
+          </Col>
+        </Row>
+        <SourceForm
+          buttonLabel={intl.formatMessage(localMessages.addButton)}
+          onSave={handleSave}
+          mediaMetadataSetsByName={mediaMetadataSetsByName}
+        />
+      </Grid>
+    </Permissioned>
+  </div>
+);
 
 CreateSourceContainer.propTypes = {
   // from context
@@ -53,7 +49,7 @@ CreateSourceContainer.propTypes = {
   mediaMetadataSetsByName: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = () => ({
+const mapStateToProps = (state) => ({
   mediaMetadataSetsByName: state.system.staticTags.tagSets.mediaMetadataSetsByName,
 });
 

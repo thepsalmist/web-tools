c7829124eb0ef80cbccd9d436482446977f805a8
Dennis Jen <dsjen@media.mit.edu>
2021-05-05 12:57:10 -0400

    Change source feeds to display by type (e.g. feed, podcast) * added in stub for sitemaps (only visible to admins)

--- src/components/source/SourceFeedTable.js
+++ src/components/source/SourceFeedTable.js
@@ -1,17 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Link from 'react-router/lib/Link';
+import Alert from '@material-ui/lab/Alert';
 import { FormattedMessage, FormattedDate, injectIntl } from 'react-intl';
 import messages from '../../resources/messages';
-import { EditButton } from '../common/IconButton';
-import TabSelector from '../common/TabSelector';
 import { PERMISSION_MEDIA_EDIT } from '../../lib/auth';
 import Permissioned from '../common/Permissioned';
 import withHelp from '../common/hocs/HelpfulContainer';
 
 const localMessages = {
-  activeTabLabel: { id: 'source.feed.active', defaultMessage: 'Active ({num})' },
-  inactiveTabLabel: { id: 'source.feed.inactive', defaultMessage: 'Disabled ({num})' },
   lastNewStory: { id: 'source.feed.lastNewStory', defaultMessage: 'Last New Story' },
   lastDownloadAttempt: { id: 'source.feed.lastDownloadAttempt', defaultMessage: 'Last Attempt' },
   lastDownloadSuccess: { id: 'source.feed.lastDownloadSuccess', defaultMessage: 'Last Success' },
@@ -19,77 +16,46 @@ const localMessages = {
   helpText: { id: 'source.feed.help.text',
     defaultMessage: '<p>A Spider Feed is a source found via spidering. When a story is found via spidering, as opposed to RSS publishing, Media Cloud creates a system generated source.</p>',
   },
+  emptyIngest: { id: 'source.feeds.table.empty', defaultMessage: 'No {feedStatus} Ingests' },
 };
 
 class SourceFeedTable extends React.Component {
-  state = {
-    selectedViewIndex: 0,
-  };
-
   isSystemGenerated = (feed) => feed.name === 'Spider Feed' || feed.url.endsWith('#spidered');
 
   render() {
-    const { feeds, helpButton } = this.props;
-    const { formatMessage } = this.props.intl;
-    const content = null;
+    const { feeds, helpButton, feedStatus } = this.props;
 
-    if (feeds === undefined) {
+    if (!feeds || feeds.length === 0) {
       return (
-        <div>
-          { content }
-        </div>
+        <>
+          <Alert severity="info"><FormattedMessage {...localMessages.emptyIngest} values={{ feedStatus }} /></Alert>
+        </>
       );
-    }
-
-    const sortedFeeds = feeds.sort((a, b) => b.lastNewStoryMoment - a.lastNewStoryMoment);
-    const activeFeeds = sortedFeeds.filter(f => f.active);
-    const inactiveFeeds = sortedFeeds.filter(f => !f.active);
-
-    let tabFeeds;
-    switch (this.state.selectedViewIndex) {
-      case 0:
-        tabFeeds = activeFeeds;
-        break;
-      case 1:
-        tabFeeds = inactiveFeeds;
-        break;
-      default:
-        break;
     }
 
     return (
       <div className="source-feed-table">
-        <TabSelector
-          tabLabels={[
-            formatMessage(localMessages.activeTabLabel, { num: activeFeeds.length }),
-            formatMessage(localMessages.inactiveTabLabel, { num: inactiveFeeds.length }),
-          ]}
-          onViewSelected={index => this.setState({ selectedViewIndex: index })}
-        />
         <table width="100%">
           <tbody>
             <tr>
               <th><FormattedMessage {...messages.feedName} /></th>
-              <th><FormattedMessage {...messages.feedType} /></th>
-              <th><FormattedMessage {...messages.feedIsActive} /></th>
               <th><FormattedMessage {...messages.feedUrl} /></th>
               <th><FormattedMessage {...localMessages.lastNewStory} /></th>
               <th><FormattedMessage {...localMessages.lastDownloadAttempt} /></th>
               <th><FormattedMessage {...localMessages.lastDownloadSuccess} /></th>
-              <th />
             </tr>
-            {tabFeeds.map((feed, idx) => (
+            {feeds.map((feed, idx) => (
               <tr key={feed.feeds_id} className={`${(idx % 2 === 0) ? 'even' : 'odd'} feed-${(feed.active) ? 'active' : 'disabled'}`}>
                 <td>
                   {this.isSystemGenerated(feed) && <i>System Generated {' '}</i>}
                   {feed.name}
                   {this.isSystemGenerated(feed) && helpButton}
-                </td>
-                <td>
-                  {feed.type}
-                </td>
-                <td>
-                  { feed.active ? <FormattedMessage {...messages.feedIsActive} /> : <FormattedMessage {...messages.feedIsDisabled} /> }
+                  <Permissioned onlyRole={PERMISSION_MEDIA_EDIT}>
+                    { ' | ' }
+                    <Link to={`/sources/${feed.media_id}/feeds/${feed.feeds_id}/edit`}>
+                      edit
+                    </Link>
+                  </Permissioned>
                 </td>
                 <td>
                   <a href={feed.url}>{feed.url}</a>
@@ -102,13 +68,6 @@ class SourceFeedTable extends React.Component {
                 </td>
                 <td>
                   {feed.lastSuccessfulDownloadMoment && <FormattedDate value={feed.lastSuccessfulDownloadMoment} />}
-                </td>
-                <td>
-                  <Permissioned onlyRole={PERMISSION_MEDIA_EDIT}>
-                    <Link to={`/sources/${feed.media_id}/feeds/${feed.feeds_id}/edit`}>
-                      <EditButton />
-                    </Link>
-                  </Permissioned>
                 </td>
               </tr>
             ))}
@@ -123,6 +82,7 @@ SourceFeedTable.propTypes = {
   feeds: PropTypes.array,
   intl: PropTypes.object.isRequired,
   helpButton: PropTypes.node.isRequired,
+  feedStatus: PropTypes.string.isRequired,
 };
 
 export default injectIntl(
--- src/components/source/mediaSource/SourceFeedContainer.js
+++ src/components/source/mediaSource/SourceFeedContainer.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
-import { Grid, Row, Col } from 'react-flexbox-grid/lib';
+import { Grid } from '@material-ui/core';
 import Link from 'react-router/lib/Link';
 import { push } from 'react-router-redux';
 import { fetchSourceFeeds, scrapeSourceFeeds, fetchSourceDetails } from '../../../actions/sourceActions';
@@ -10,76 +10,125 @@ import withAsyncData from '../../common/hocs/AsyncDataContainer';
 import MediaSourceIcon from '../../common/icons/MediaSourceIcon';
 import SourceFeedTable from '../SourceFeedTable';
 import messages from '../../../resources/messages';
-import { DownloadButton, AddButton } from '../../common/IconButton';
+import { DownloadButton } from '../../common/IconButton';
 import AppButton from '../../common/AppButton';
 import Permissioned from '../../common/Permissioned';
-import { PERMISSION_MEDIA_EDIT } from '../../../lib/auth';
+import { getUserRoles, hasPermissions, PERMISSION_MEDIA_EDIT } from '../../../lib/auth';
 import { updateFeedback } from '../../../actions/appActions';
 import { SOURCE_SCRAPE_STATE_QUEUED, SOURCE_SCRAPE_STATE_RUNNING } from '../../../reducers/sources/sources/selected/sourceDetails';
 import PageTitle from '../../common/PageTitle';
+import TabSelector from '../../common/TabSelector';
 
 const localMessages = {
   pageTitle: { id: 'source.feeds.pageTitle', defaultMessage: 'Feeds' },
   title: { id: 'source.feeds.title', defaultMessage: '{name} | Source Feeds | Media Cloud' },
   sourceFeedsTitle: { id: 'source.details.feeds.title', defaultMessage: '{name}: Feeds' },
-  add: { id: 'source.deatils.feeds.add', defaultMessage: 'Add A Feed' },
+  add: { id: 'source.details.feeds.add', defaultMessage: 'Add A Feed' },
+  activeLabel: { id: 'source.details.feed.active', defaultMessage: 'Active' },
+  inactiveLabel: { id: 'source.details.feed.inactive', defaultMessage: 'Inactive' },
+  rssFeeds: { id: 'source.details.feed.rssFeeds', defaultMessage: 'RSS Feeds' },
+  podcasts: { id: 'source.details.feed.podcasts', defaultMessage: 'Podcasts' },
+  sitemaps: { id: 'source.details.feed.sitemaps', defaultMessage: 'Sitemaps' },
 };
 
 class SourceFeedContainer extends React.Component {
+  state = {
+    selectedViewIndex: 0,
+  };
+
   downloadCsv = () => {
     const { sourceId } = this.props;
     const url = `/api/sources/${sourceId}/feeds/feeds.csv`;
     window.location = url;
   }
 
+  getSelectedFeedType = () => {
+    const { selectedViewIndex } = this.state;
+    const feedTypes = {
+      0: 'syndicated',
+      1: 'podcast',
+      2: 'sitemaps',
+    };
+    return feedTypes[selectedViewIndex];
+  }
+
+  getFeeds = (isActive) => {
+    const { feeds } = this.props;
+    const sortedFeeds = feeds.sort((a, b) => b.lastNewStoryMoment - a.lastNewStoryMoment);
+    const filteredFeedTypes = sortedFeeds.filter(f => f.type === this.getSelectedFeedType());
+    return filteredFeedTypes.filter(f => f.active === isActive);
+  }
+
   render() {
-    const { sourceId, sourceName, feeds, scrapeFeeds, pushToUrl } = this.props;
+    const { sourceId, sourceName, feeds, scrapeFeeds, pushToUrl, user } = this.props;
     const { formatMessage } = this.props.intl;
-    const content = null;
     if (feeds === undefined) {
       return (
-        <div>
-          { content }
-        </div>
+        <div />
       );
     }
+    const tabLabels = [
+      formatMessage(localMessages.rssFeeds),
+      formatMessage(localMessages.podcasts),
+    ];
+    // ungated this feature once sitemaps functionality is released
+    if (hasPermissions(getUserRoles(user), PERMISSION_MEDIA_EDIT)) {
+      tabLabels.push(formatMessage(localMessages.sitemaps));
+    }
     return (
-      <Grid className="details source-details">
-        <PageTitle value={[localMessages.pageTitle, sourceName]} />
-        <Row>
-          <Col lg={11} xs={11}>
-            <h1>
-              <MediaSourceIcon height={32} />
-              <Link to={`/sources/${sourceId}`}>
-                <FormattedMessage {...localMessages.sourceFeedsTitle} values={{ name: sourceName }} />
-              </Link>
-            </h1>
-            <Permissioned onlyRole={PERMISSION_MEDIA_EDIT}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <PageTitle value={[localMessages.pageTitle, sourceName]} />
+        </Grid>
+        <Grid item xs={9}>
+          <h1>
+            <MediaSourceIcon height={32} />
+            <Link to={`/sources/${sourceId}`}>
+              <FormattedMessage {...localMessages.sourceFeedsTitle} values={{ name: sourceName }} />
+            </Link>
+          </h1>
+        </Grid>
+        <Grid item xs={3}>
+          <div className="actions" style={{ marginTop: 40 }}>
+            <DownloadButton tooltip={formatMessage(messages.download)} onClick={this.downloadCsv} />
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <TabSelector
+            tabLabels={tabLabels}
+            onViewSelected={index => this.setState({ selectedViewIndex: index })}
+          />
+        </Grid>
+        <Permissioned onlyRole={PERMISSION_MEDIA_EDIT}>
+          <Grid container item spacing={1}>
+            { this.getSelectedFeedType() === 'syndicated' && (
+              <Grid item>
+                <AppButton
+                  className="source-scrape-feeds-button"
+                  label={formatMessage(messages.scrapeForFeeds)}
+                  color="primary"
+                  onClick={scrapeFeeds}
+                />
+              </Grid>
+            )}
+            <Grid item>
               <AppButton
                 className="source-scrape-feeds-button"
-                label={formatMessage(messages.scrapeForFeeds)}
+                label={formatMessage(localMessages.add)}
                 color="primary"
-                onClick={scrapeFeeds}
+                onClick={() => { pushToUrl(`/sources/${sourceId}/feeds/create`); }}
               />
-            </Permissioned>
-          </Col>
-          <Col lg={1} xs={1}>
-            <div className="actions" style={{ marginTop: 40 }}>
-              <Permissioned onlyRole={PERMISSION_MEDIA_EDIT}>
-                <AddButton
-                  tooltip={formatMessage(localMessages.add)}
-                  onClick={() => { pushToUrl(`/sources/${sourceId}/feeds/create`); }}
-                />
-              </Permissioned>
-              <DownloadButton tooltip={formatMessage(messages.download)} onClick={this.downloadCsv} />
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={12}>
-            <SourceFeedTable feeds={feeds} />
-          </Col>
-        </Row>
+            </Grid>
+          </Grid>
+        </Permissioned>
+        <Grid item xs={12}>
+          <h2>{formatMessage(localMessages.activeLabel)}</h2>
+          <SourceFeedTable feeds={this.getFeeds(true)} feedStatus="Active" />
+        </Grid>
+        <Grid item xs={12}>
+          <h2>{formatMessage(localMessages.inactiveLabel)}</h2>
+          <SourceFeedTable feeds={this.getFeeds(false)} feedStatus="Inactive" />
+        </Grid>
       </Grid>
     );
   }
@@ -98,6 +147,7 @@ SourceFeedContainer.propTypes = {
   sourceName: PropTypes.string.isRequired,
   feeds: PropTypes.array,
   feedcount: PropTypes.number,
+  user: PropTypes.object,
 };
 
 const mapStateToProps = (state, ownProps) => ({
@@ -106,6 +156,7 @@ const mapStateToProps = (state, ownProps) => ({
   fetchStatus: state.sources.sources.selected.feed.feeds.fetchStatus,
   feeds: state.sources.sources.selected.feed.feeds.list,
   feedcount: state.sources.sources.selected.feed.feeds.count,
+  user: state.user,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
